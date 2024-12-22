c04575c423871068c4cab4279a886a6e18d8e751
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-12-22 19:45:55 +0530

    added pageable and sort

--- src/test/java/com/jpa/spring_jpa/repositry/CourseRepositoryTest.java
+++ src/test/java/com/jpa/spring_jpa/repositry/CourseRepositoryTest.java
@@ -8,10 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.jpa.spring_jpa.entity.Course;
 import com.jpa.spring_jpa.entity.CourseMaterial;
 import com.jpa.spring_jpa.entity.Teacher;
+
+import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
 
 @SpringBootTest
 class CourseRepositoryTest {
@@ -35,5 +39,18 @@ class CourseRepositoryTest {
 		crepo.save(cr);
 
 	}
-
+	@Test
+	public void findAllPagination() {
+	            Pageable firstPageableWithThreeRec =PageRequest.of(0, 3);
+	            Pageable secondPageWithTwoRecords =PageRequest.of(1, 2);
+	            List<Course> courses=crepo.findAll(firstPageableWithThreeRec).getContent();
+	            System.out.println("courses = "+ courses);
+	}
+    
+	@Test
+	public void findAllBySorting() {
+		Pageable sortByTitle= PageRequest.of(0,2,org.springframework.data.domain.Sort.by("title"));
+		List<Course> courses=crepo.findAll(sortByTitle).getContent();
+		System.out.println("sorted"+courses);
+	}
 }
